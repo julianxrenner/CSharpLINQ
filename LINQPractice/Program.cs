69ed98c9a57d07e0c0b5d9dc48cc278241a13c19
julianxrenner <julesman18@gmail.com>
2026-05-07 05:33:49 -0400

    messing around with LINQ

---
+++ LINQPractice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+var myBuildings = new List<Building>{
+  new (1000, 10, "Small Buidling"),
+  new(10000, 7, "Big Buidling"),
+  new(100000, 5, "Bigger Builing")
+};
+
+var calculatedTaxes = myBuildings.Select(building => building.TaxPerSize * building.Size);
+
+foreach (var tax in calculatedTaxes)
+{
+  Console.WriteLine(tax);
+}
+
+record Building(int Size, int TaxPerSize, string Name);
